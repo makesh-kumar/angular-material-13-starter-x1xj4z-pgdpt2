cf587d9ddde13c21de9319224e8956433ff85298
makesh-kumar <git@stackblitz.com>
2024-10-03 20:00:52 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-material-13-starter-x1xj4z-pgdpt2
+
+[Edit in StackBlitz next generation editor ⚡️](https://stackblitz.com/~/github.com/makesh-kumar/angular-material-13-starter-x1xj4z-pgdpt2)
---
+++ src/app/app.module.ts
@@ -0,0 +1,57 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule, HammerGestureConfig } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatIconModule } from '@angular/material/icon';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatTabsModule } from '@angular/material/tabs';
+import { HammerModule, HAMMER_GESTURE_CONFIG } from '@angular/platform-browser';
+import * as Hammer from 'hammerjs';
+import { AppComponent } from './app.component';
+import { ProfileListComponent } from './daily-recommendations/profile-list/profile-list.component';
+import { ProfileCardComponent } from './daily-recommendations//profile-card/profile-card.component';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { DailyRecommendationsComponent } from './daily-recommendations/daily-recommendations.component';
+import { PendingProfilesComponent } from './pending-profiles/pending-profiles.component';
+import { PendingProfileListComponent } from './pending-profiles/pending-profile-list/pending-profile-list.component';
+
+export class MyHammerConfig extends HammerGestureConfig {
+  overrides = {
+    swipe: {
+      direction: Hammer.DIRECTION_HORIZONTAL,
+      threshold: 10,
+      velocity: 0.3,
+    },
+  };
+}
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ProfileListComponent,
+    ProfileCardComponent,
+    DailyRecommendationsComponent,
+    PendingProfilesComponent,
+    PendingProfileListComponent,
+  ],
+  bootstrap: [AppComponent],
+  providers: [{ provide: HAMMER_GESTURE_CONFIG, useClass: MyHammerConfig }],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    FormsModule,
+    HammerModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatCardModule,
+    MatDialogModule,
+    MatIconModule,
+    MatSnackBarModule,
+    MatTabsModule,
+  ],
+})
+export class AppModule {}
